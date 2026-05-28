493f1f5fec7c7f38501d2b87c11b74667e4aef14
sloth-dotcom <sloth@colourbox.com>
2026-05-28 09:19:48 +0200

    Embed visuel email-designer i kampagne-workspace

---
+++ colourbox-skyfish-email/_next/static/chunks/app/content-builder/page-da834e198e0a8754.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[618],{3130:function(n,e,u){Promise.resolve().then(u.bind(u,9702))}},function(n){n.O(0,[702,971,117,744],function(){return n(n.s=3130)}),_N_E=n.O()}]);
